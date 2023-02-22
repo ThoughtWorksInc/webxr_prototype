8044716f77b8b422e14ce8e8248cf3c9f2278eba
Debora Beda <dbeda@thoughtworks.com>
2023-02-22 13:10:52 -0300

    Menu to scenes - Beda

---
+++ Assets/Scenes/Menu/MenuScript.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject buttonTemplate;
+
+    private string[] scenes = new string[]
+    {
+        "TrainerScene",
+        "TraineeScene",
+        "SpikeMenu"
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject g;
+
+        foreach (string scene in scenes)
+        {
+            g = Instantiate(buttonTemplate, transform);
+            g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = scene;
+            g.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(scene));
+        }
+
+        Destroy(buttonTemplate);
+    }
+}
