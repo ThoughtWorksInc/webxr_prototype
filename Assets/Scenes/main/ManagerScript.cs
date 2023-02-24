4a5e009a1b12d7ab384c21ea2f6a11af0e7687b7
Vandana barsaiyan <vandana.barsaiyan@thoughtworks.com>
2023-02-24 16:54:13 +0530

    Fix text/image overlay bug- Vandana, Tanvi

--- Assets/Scenes/main/ManagerScript.cs
+++ Assets/Scenes/main/ManagerScript.cs
@@ -20,24 +20,37 @@ public class ManagerScript : MonoBehaviour
     public void SaveScene(){
         Debug.Log("Save scene");
         OverlaysData overlayData = new OverlaysData();
-
+        
+        // Debug.Log("Textover"+GameObject.FindGameObjectsWithTag("Text_Overlay"));
+        // Debug.Log("Textover"+GameObject.FindGameObjectsWithTag("Image_Overlay"));
+        string textPrefab = "Text";
+        GameObject textGameObject = Resources.Load(textPrefab) as GameObject;
+        if(textGameObject!=null){
         foreach (var gameobj in GameObject.FindGameObjectsWithTag("Text_Overlay"))
-        {
+        {  
             OverlayGameObject overlayGameObject = new OverlayGameObject();
             overlayGameObject.type = "Text";
             overlayGameObject.position = gameobj.transform.position;
             overlayGameObject.scale = gameobj.transform.localScale;
             overlayGameObject.rotation = gameobj.transform.rotation;
             overlayData.myOverlays.Add(overlayGameObject);
+        
+        
         }
+        }
+        string imagePrefab = "Image";
+        GameObject imageGameObject = Resources.Load(imagePrefab) as GameObject;
+        if(imageGameObject!=null){
         foreach (var gameobj in GameObject.FindGameObjectsWithTag("Image_Overlay"))
-        {
+        {   
             OverlayGameObject overlayGameObject = new OverlayGameObject();
             overlayGameObject.type = "Image";
             overlayGameObject.position = gameobj.transform.position;
             overlayGameObject.scale = gameobj.transform.localScale;
             overlayGameObject.rotation = gameobj.transform.rotation;
             overlayData.myOverlays.Add(overlayGameObject);
+        
+        }
         }
 
         string json = JsonUtility.ToJson(overlayData);
