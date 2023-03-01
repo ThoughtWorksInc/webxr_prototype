3c656987a0c77625d6862bc170e9a85ed7c64430
Debora Beda <dbeda@thoughtworks.com>
2023-03-01 10:46:18 -0300

    File Browsing webgl WIP - Beda, Thibault, Natalie, Tanvi, Vandana

--- Assets/Scripts/Menus/ImageInputPanelScript.cs
+++ Assets/Scripts/Menus/ImageInputPanelScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEditor;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class ImageInputPanelScript : MonoBehaviour, IInputPanel
 {
@@ -13,7 +15,11 @@ public class ImageInputPanelScript : MonoBehaviour, IInputPanel
     public Button saveButton;
 
     private string imagePath = "";
-    private Image image; 
+    private Image image;
+
+    void Start()
+    {
+    }
 
     public void ShowInputPanel(GameObject referenceObj)
     {
@@ -21,18 +27,33 @@ public class ImageInputPanelScript : MonoBehaviour, IInputPanel
         overlay = referenceObj;
         title.text = "Edit Image for: " + overlay.name;
 
-        imagePath = AssetDatabase.GetAssetPath(overlay.GetComponentInChildren<Image>().sprite);    
+#if UNITY_EDITOR
+        imagePath = AssetDatabase.GetAssetPath(overlay.GetComponentInChildren<Image>().sprite);
+#endif
     }
 
     public void OpenFileExplorer()
     {
-        imagePath = UnityEditor.EditorUtility.OpenFilePanel("Select image", "", "png,jpg,jpeg");
+#if UNITY_EDITOR
+        imagePath = EditorUtility.OpenFilePanel("Select image", "", "png,jpg,jpeg");
+#else
+       FileUploaderHelper.RequestFile((path) => 
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return;
+
+            imagePath = path;
+            pathText.text = imagePath;
+            saveButton.interactable = true;
+        });
+#endif
+
         pathText.text = imagePath;
-        saveButton.interactable = true;
     }
 
     public void SaveImageInput()
     {
+#if UNITY_EDITOR
         if (imagePath.Length != 0)
         {
             // Load the image from file
@@ -44,12 +65,41 @@ public class ImageInputPanelScript : MonoBehaviour, IInputPanel
             overlay.GetComponentInChildren<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
             Debug.Log(overlay.GetComponentInChildren<Image>().sprite);
         }
+#else
+        StartCoroutine(UploadImage(imagePath));  
+#endif
 
         imagePath = "";
         overlay = null;
 
         saveButton.interactable = false;
         this.gameObject.SetActive(false);
+    }
+
+    // Coroutine for image upload
+    IEnumerator UploadImage(string path)
+    {
+        // This is where the texture will be stored.
+        Texture2D texture;
+
+        // using to automatically call Dispose, create a request along the path to the file
+        using (UnityWebRequest imageWeb = new UnityWebRequest(path, UnityWebRequest.kHttpVerbGET))
+        {
+            // We create a "downloader" for textures and pass it to the request
+            imageWeb.downloadHandler = new DownloadHandlerTexture();
+
+            // We send a request, execution will continue after the entire file have been downloaded
+            yield return imageWeb.SendWebRequest();
+
+            // Getting the texture from the "downloader"
+            texture = ((DownloadHandlerTexture)imageWeb.downloadHandler).texture;
+        }
+
+        // Create a sprite from a texture and pass it to the avatar image on the UI
+        overlay.GetComponentInChildren<Image>().sprite = Sprite.Create(
+            texture,
+            new Rect(0.0f, 0.0f, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f));
     }
 
     private Texture2D LoadTexture(string path)
